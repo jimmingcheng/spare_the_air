a530126e97ef2c896cf800bd3c0476da52a7a1a7
Jimming Cheng <jimming@gmail.com>
2019-08-26 22:36:42 -0700

    use widget API instead of scraping page

--- spare_the_air/location/sf.py
+++ spare_the_air/location/sf.py
@@ -1,43 +1,23 @@
-from datetime import datetime
-from datetime import timedelta
-from datetime import tzinfo
+import json
 import re
-
 from botocore.vendored import requests
 
 
+SPARE_THE_AIR_URL = 'http://www.baaqmd.gov/Presentation/DotGov/Webservices/Widgets/AirQualityStatus.ashx?key=117c43da-3f15-453e-9a46-7d0bdb772241'
+
+
 def get_burn_status():
-    resp = requests.get('http://www.sparetheair.org')
-    html = resp.text
-    message_html = re.search(r'<div[^>]+sta-day[^>]*>(.*?)<div[^>]+sta-day', html, re.DOTALL).group(1)
-    matches = re.search(r'<div[^>]*>(.*?)</div>.*?<a [^>]*>(.*)</a>', message_html, re.DOTALL)
-    now = datetime.now(PDT())
-    message_date = datetime.strptime(matches.group(1) + '/' + str(now.year), '%A, %m/%d/%Y')
-
-    message_text = remove_tags(matches.group(2))
-    if message_text.lower() == 'no spare the air alert in effect':
-        message_text = 'It\'s OK to burn'
-    elif message_text.lower() == 'winter spare the air alert in effect':
-        message_text = 'It\'s not legal to burn'
-
-    if message_date.toordinal() == now.toordinal():
-        message_text += ' today'
-    else:
-        message_text += ' for ' + message_date.strftime('%A, %B %-d')
-
-    return message_text
-
-
-class PDT(tzinfo):
-    def utcoffset(self, dt):
-        return timedelta(hours=-7)
-
-    def tzname(self, dt):
-        return "PDT"
-
-    def dst(self, dt):
-        return timedelta(hours=-7)
-
-
-def remove_tags(text):
-    return re.sub(r'\s+', ' ', re.sub(r'<[^>]+>', ' ', text)).strip()
+    """
+    Sample response:
+    ({"Title":"SPARE THE AIR STATUS","AlertLink":"http://www.sparetheair.org/about/what-is-spare-the-air","AlertMode":"SummerWidgetNoAlertContent","AlertContent":"No Spare the Air Alert in Effect","Date":"Monday, 8/26","WidgetsLink":"http://www.baaqmd.gov/online-services/air-quality-widgets","WidgetsLinkDescription":"Air Quality Widgets"});
+    """
+    resp = requests.get(SPARE_THE_AIR_URL, headers={'Host': 'scooterbot.org'})
+    matches = re.match(r'^\((.+)\);$', resp.text)
+    if matches:
+        json_data = json.loads(matches[1])
+        message_text = json_data['AlertContent']
+        if message_text.lower() == 'no spare the air alert in effect':
+            return "It's OK to burn today"
+        elif message_text.lower() == 'winter spare the air alert in effect':
+            return "It's not legal to burn today"
+    return "I don't know what to say"
